6827501eea2a41f05d74f0fbb8278c9eddf87d00
Zukka <zukka75@gmail.com>
2020-12-08 21:52:09 +0100

    Add appBarConfiguration

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.viewmodel.ShoeViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
@@ -16,19 +19,24 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ShoeViewModel
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         Timber.plant(Timber.DebugTree())
+        appBarConfiguration = AppBarConfiguration
+            .Builder(R.id.shoeListFragment, R.id.loginFragment)
+            .build()
         viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
         viewModel.isLogged.observe(this, Observer { logged ->
             if (logged)
                 Navigation.findNavController(this, R.id.main_navigation).navigate(R.id.action_loginFragment_to_welcomeFragment)
         })
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-     }
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.home_menu, menu)
@@ -45,5 +53,16 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null as DrawerLayout?)
+    }
+
+    override fun onBackPressed() {
+        if (appBarConfiguration.topLevelDestinations.contains(navController.currentDestination?.id))
+            finish()
+        else
+            onSupportNavigateUp()
     }
 }
